f2869dc7f6183f74abc3f9ed1a54b470aba7aa1e
leon <leonluis_23@yahoo.es>
2019-08-05 21:34:47 -0400

    navbar with items

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './Logo.css'
+const logo = ()=>(
+
+    <div className= { classes.Logo }>
+        <img src="images/burger-logo.png" alt="BurguerApp Logo"/> 
+    </div>   
+);
+export default logo;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavItem from './NavigationItem/NavItem';
+import classes from './NavigationItems.css'
+
+const navItems = ()=>(
+
+    <ul className = { classes.NavigationItems }>
+
+        <NavItem link="/" active > Burger Builder </NavItem>
+        <NavItem link="/"  > Checkout </NavItem>
+
+    </ul>
+);
+export default navItems;
